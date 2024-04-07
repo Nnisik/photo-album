a6d93ce7a730afdaa51a1d307553e44dd14f0208
Nnisik <nisik0120@gmail.com>
2024-04-08 01:31:44 +0300

    staerted working on galery filling from json file

---
+++ js/gallery.js
@@ -0,0 +1,39 @@
+import postsData from '../data/posts.json';
+
+const drawPostElement = (header, liked, img) => {
+    // post container
+    const newElement = document.createElement("section");
+    newElement.className = "post";
+    // post image
+    const postImage = document.createElement("img");
+    postImage.src = "./assets/IMG_1286.JPG";
+    postImage.alt = "post";
+    postImage.className = "post-img";
+    newElement.appendChild(postImage);
+    // post delete button
+    const postDeleteButton = document.createElement("button");
+    postDeleteButton.className = "delete-btn";
+    newElement.appendChild(postDeleteButton);
+    // post information section
+    const postsInfoSection = document.createElement("section");
+    postsInfoSection.className = "post-info";
+    const postHeader = document.createElement("h2");
+    postHeader.className = "post-header";
+    postHeader.innerHTML = header;
+    postsInfoSection.appendChild(postHeader);
+    const likeStatus = document.createElement("div");
+    likeStatus.className = "like-btn";
+    if (liked) {
+        likeStatus.style.background = "red";
+    }
+    else {
+        likeStatus.style.background = "white";
+    }
+    postsInfoSection.appendChild(likeStatus);
+    newElement.appendChild(postsInfoSection);
+}
+
+const galleryElement = document.getElementById("gallery");
+postsData.reduce((post) => {
+    drawPostElement(post.name, post.like, post.image);
+});
