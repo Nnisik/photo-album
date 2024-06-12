07f897a473907922693872171b903ef8258cc568
Nnisik <nisik0120@gmail.com>
2024-06-12 13:14:47 +0300

    added svg like icon + separated mobile layout

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,41 +1,49 @@
+function drawNewPost(postInfo) {
+  // post container
+  const postContainer = document.createElement("section");
+  postContainer.setAttribute("data-id", postInfo.id);
+  postContainer.classList.add("post");
+  
+  // post image const
+  const postImage = document.createElement("img");
+  postImage.src = postInfo.image;
+  postImage.classList.add("post-img");
+  postContainer.appendChild(postImage);
+  
+  // post delete button
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-btn");
+  deleteBtn.setAttribute("data-id", postInfo.id);
+  postContainer.appendChild(deleteBtn);
+  
+  // post information section
+  const postInfoSection = document.createElement("section");
+  postInfoSection.classList.add("post-info");
+  
+  // post header
+  const postHeader = document.createElement("h2");
+  postHeader.classList.add("post-header");
+  postHeader.textContent = postInfo.name;
+  postInfoSection.appendChild(postHeader);
+  
+  // like button
+  const likeBtn = document.createElement("input");
+  likeBtn.type = "checkbox";
+  likeBtn.setAttribute("id", `post-${postInfo.id}`);
+  likeBtn.classList.add("like-btn");
+  
+  postInfoSection.appendChild(likeBtn);
+  postContainer.appendChild(postInfoSection);
+  return postContainer;
+}
+
 export default async function drawGallery() {
-  const JSONFilePath = '../src/data/data-posts.json';
+  const JSONFilePath = "../src/data/data-posts.json";
   fetch(JSONFilePath)
     .then((response) => response.json())
     .then((json) => {
       json.map((post) => {
-        // post container
-        const postContainer = document.createElement("section");
-        postContainer.setAttribute("data-id", post["id"]);
-        postContainer.classList.add("post");
-        // post image const
-        const postImage = document.createElement("img");
-        postImage.src = post["image"];
-        postImage.classList.add("post-img");
-        postContainer.appendChild(postImage);
-        // post delete button
-        const deleteBtn = document.createElement("button");
-        deleteBtn.classList.add("delete-btn");
-        deleteBtn.setAttribute("data-id", post["id"]);
-        postContainer.appendChild(deleteBtn);
-        // post information section
-        const postInfoSection = document.createElement("section");
-        postInfoSection.classList.add("post-info");
-        // post header
-        const postHeader = document.createElement("h2");
-        postHeader.classList.add("post-header");
-        postHeader.textContent = post["name"];
-        postInfoSection.appendChild(postHeader);
-        // like button
-        const likeBtn = document.createElement("div");
-        likeBtn.classList.add("like-btn");
-        likeBtn.setAttribute("data-like", post["like"]);
-        if (post["like"]) {
-          likeBtn.classList.toggle("liked");
-        }
-        postInfoSection.appendChild(likeBtn);
-        postContainer.appendChild(postInfoSection);
-        document.querySelector("#gallery").appendChild(postContainer);
+        document.querySelector("#gallery").appendChild(drawNewPost(post));
       });
-  });
+    });
 }
